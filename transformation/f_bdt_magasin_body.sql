86eb1f42ba3d0bd69bdaf90d1639ac0c42e69ae0
Mooduku <julien.amalfi@gmail.com>
2014-03-23 15:37:50 +0100

    Create f_bdt_magasin_body.sql

---
+++ transformation/f_bdt_magasin_body.sql
@@ -0,0 +1,49 @@
+create or replace type body nf26p062.f_bdt_magasin_type
+is
+    member function getNumMag retun varchar
+    is
+    begin
+        return num_mag;
+    end;
+
+    member function getRayonBs return char
+    is
+    begin
+        return rayon_bs;
+    end;
+
+    member function getRayonRecent return vchar
+    is
+    begin
+        return rayon_r;
+    end;
+
+    member function getDpt return char
+    is
+    begin
+        return dpt;
+    end;
+
+    member function getDptNom return char
+    is
+    begin
+        return dpt_nom;
+    end;
+
+    member function getDptPop return varchar
+    is
+    begin
+        return dpt_pop;
+    end;
+
+    member function getRayonnage return varchar
+    is
+    begin
+        return rayonnage;
+    end;
+    member function getRayonnageExt return varchar
+    is
+    begin
+        return rayonnage_ext;
+    end;
+end;
