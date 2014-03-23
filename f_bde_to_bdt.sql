928c6de4387c06c471505e566e834d0ca1eaa1f3
Benjamin Fradet <benjamin.fradet@gmail.com>
2014-03-23 17:05:55 +0100

    bde->bdt and bdt->dw

--- f_bde_to_bdt.sql
+++ f_bde_to_bdt.sql
@@ -6,9 +6,11 @@ drop index f_bdt_catalogue_isbn_idx;
 drop index f_bdt_magasin_num_mag_idx;
 
 insert into nf26p008.f_bdt_catalogue select * from nf26p008.f_bde_catalogue;
+commit;
 
 insert into nf26p008.f_bdt_date(dat)
    select dateAchat from nf26p008.f_bde_ventes;
+commit;
 
 insert into nf26p008.f_bdt_magasin (
     num_mag,
@@ -31,8 +33,8 @@ select
     market_view.rayonnage_ext
 from nf26p008.f_bdt_marketing_view market_view
 join nf26p008.f_bde_departement departement
-on departement.dpt = market_view.dpt
-;
+on departement.dpt = market_view.dpt;
+commit;
 
 insert into nf26p008.f_bdt_ventes (
     magasin, 
@@ -46,14 +48,20 @@ select
     dateAchat,
     ticketnumber
 from nf26p008.f_bde_ventes ventes;
+commit;
 
 create unique index f_bdt_catalogue_idx_isbn on nf26p008.f_bdt_catalogue(isbn);
 create unique index f_bdt_magasin_idx_num_mag 
     on nf26p008.f_bdt_magasin(num_mag);
 
-alter table nf26p008.f_bdt_catalogue enable constraint f_bdt_catalogue_pk_isbn;
-alter table nf26p008.f_bdt_magasin enable constraint f_bdt_magasin_pk_num_mag;
-alter table nf26p008.f_bdt_ventes enable constraint f_bdt_ventes_fk_dat;
+alter table nf26p008.f_bdt_catalogue enable constraint f_bdt_catalogue_pk_isbn
+    exceptions into nf26p008.exceptions;
+alter table nf26p008.f_bdt_magasin enable constraint f_bdt_magasin_pk_num_mag
+    exceptions into nf26p008.exceptions;
+alter table nf26p008.f_bdt_ventes enable constraint f_bdt_ventes_fk_dat
+    exceptions into nf26p008.exceptions;
+
+select count(*) from nf26p008.exceptions;
 
 select * from nf26p008.f_bdt_ventes where rownum <= 10;
 select count(*) from nf26p008.f_bdt_ventes;
--- f_bdt_to_dw.sql
+++ f_bdt_to_dw.sql
@@ -1,5 +1,14 @@
---desactivation des contraintes et index dans f_dw_*
+drop index f_dw_date_idx_dat;
+drop index f_dw_magasin_idx_num_mag;
+drop index f_dw_produit_idx_isbn;
 
+alter table nf26p008.f_dw_date disable constraint f_dw_date_pk_dat;
+alter table nf26p008.f_dw_magasin disable constraint f_dw_magasin_pk_num_mag;
+alter table nf26p008.f_dw_produit disable constraint f_dw_produit_pk_isbn;
+
+alter table nf26p008.f_dw_ventes disable constraint f_dw_ventes_fk_magasin;
+alter table nf26p008.f_dw_ventes disable constraint f_dw_ventes_fk_produit;
+alter table nf26p008.f_dw_ventes disable constraint f_dw_ventes_fk_dat;
 
 insert into nf26p008.f_dw_ventes (
     magasin,
@@ -13,6 +22,7 @@ values (
         v.getProduit()
     from nf26p008.f_bdt_ventes as v
 );
+commit;
 
 insert into nf26p008.f_dw_magasin (
     num_mag,
@@ -34,6 +44,7 @@ values (
         m.getRayonnage()
     from nf26p008.f_bdt_magasin as m
 );
+commit;
 
 insert into nf26p008.f_dw_produit (
     isbn,
@@ -55,6 +66,7 @@ values (
         p.getParution()
    from nf26p008.f_bdt_produit as p
 );
+commit;
 
 insert into nf26p008.f_dw_date (
     dat,
@@ -72,7 +84,36 @@ values (
         d.getTrimestre()
     from nf26p008.f_bdt_date as d
 );
+commit;
 
---reenable constraints and indexes
+create unique index f_dw_produit_idx_isbn on nf26p008.f_dw_produit(isbn);
+create unique index f_dw_magasin_idx_num_mag on nf26p008.f_dw_magasin(num_mag);
+create unique index f_dw_date_idx_dat on nf26p008.f_dw_date(dat);
 
---test
+alter table nf26p008.f_dw_date enable constraint f_dw_date_pk_dat
+    exceptions into nf26p008.exceptions;
+alter table nf26p008.f_dw_magasin enable constraint f_dw_magasin_pk_num_mag
+    exceptions into nf26p008.exceptions;
+alter table nf26p008.f_dw_produit enable constraint f_dw_produit_pk_isbn
+    exceptions into nf26p008.exceptions;
+
+alter table nf26p008.f_dw_ventes enable constraint f_dw_ventes_fk_magasin
+    exceptions into nf26p008.exceptions;
+alter table nf26p008.f_dw_ventes enable constraint f_dw_ventes_fk_produit
+    exceptions into nf26p008.exceptions;
+alter table nf26p008.f_dw_ventes enable constraint f_dw_ventes_fk_dat
+    exceptions into nf26p008.exceptions;
+
+select count(*) from nf26p008.exceptions;
+
+select * from nf26p008.f_dw_ventes where rownum <= 10;
+select count(*) from nf26p008.f_dw_ventes;
+
+select * from nf26p008.f_dw_date where rownum <= 10;
+select count(*) from nf26p008.f_dw_date;
+
+select * from nf26p008.f_dw_magasin where rownum <= 10;
+select count(*) from nf26p008.f_dw_magasin;
+
+select * from nf26p008.f_dw_produit where rownum <= 10;
+select count(*) from nf26p008.f_dw_produit;
