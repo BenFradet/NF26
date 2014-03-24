402773280aeb8858d9ec9c3f49741ba84b42aa1a
Benjamin Fradet <benjamin.fradet@gmail.com>
2014-03-24 18:00:46 +0100

    prompts

--- f_bde_to_bdt.sql
+++ f_bde_to_bdt.sql
@@ -1,16 +1,24 @@
+prompt
+prompt enleve les indexes et constraints
 alter table nf26p008.f_bdt_catalogue disable constraint f_bdt_catalogue_pk_isbn;
 alter table nf26p008.f_bdt_magasin disable constraint f_bdt_magasin_pk_num_mag;
 
 drop index f_bdt_catalogue_isbn_idx;
 drop index f_bdt_magasin_num_mag_idx;
 
+prompt
+prompt rempli la table f_bdt_catalogue
 insert into nf26p008.f_bdt_catalogue select * from nf26p008.f_bde_catalogue;
 commit;
 
+prompt
+prompt rempli la table f_bdt_date
 insert into nf26p008.f_bdt_date(dat)
    select dateAchat from nf26p008.f_bde_ventes;
 commit;
 
+prompt
+prompt rempli la table f_bdt_magasin
 insert into nf26p008.f_bdt_magasin (
     num_mag,
     rayon_bs,
@@ -35,6 +43,8 @@ join nf26p008.f_bde_departement departement
 on departement.dpt = market_view.dpt;
 commit;
 
+prompt
+prompt rempli la table f_bdt_ventes
 insert into nf26p008.f_bdt_ventes (
     magasin, 
     produit, 
@@ -49,6 +59,8 @@ select
 from nf26p008.f_bde_ventes ventes;
 commit;
 
+prompt
+prompt recree les indexes et les constraints
 create unique index f_bdt_catalogue_idx_isbn on nf26p008.f_bdt_catalogue(isbn);
 create unique index f_bdt_magasin_idx_num_mag 
     on nf26p008.f_bdt_magasin(num_mag);
@@ -58,13 +70,24 @@ alter table nf26p008.f_bdt_catalogue enable constraint f_bdt_catalogue_pk_isbn
 alter table nf26p008.f_bdt_magasin enable constraint f_bdt_magasin_pk_num_mag
     exceptions into nf26p008.exceptions;
 
+prompt
+prompt count(*) de exceptions
 select count(*) from nf26p008.exceptions;
 
+prompt
+prompt 10 premieres lignes de f_bdt_ventes;
 select * from nf26p008.f_bdt_ventes where rownum <= 10;
+prompt count(*) de f_bdt_ventes
 select count(*) from nf26p008.f_bdt_ventes;
 
+prompt
+prompt 10 premieres lignes de f_bdt_date
 select * from nf26p008.f_bdt_date where rownum <= 10;
+prompt count(*) de f_bdt_date
 select count(*) from nf26p008.f_bdt_date;
 
+prompt
+prompt 10 premieres lignes de f_bdt_catalogue
 select * from nf26p008.f_bdt_catalogue where rownum <= 10;
+prompt count(*) de f_bdt_catalogue
 select count(*) from nf26p008.f_bdt_catalogue;
--- f_bdt_to_dw.sql
+++ f_bdt_to_dw.sql
@@ -1,3 +1,5 @@
+prompt
+prompt enleve les indexes et constraints
 alter table nf26p008.f_dw_ventes disable constraint f_dw_ventes_fk_magasin;
 alter table nf26p008.f_dw_ventes disable constraint f_dw_ventes_fk_produit;
 alter table nf26p008.f_dw_ventes disable constraint f_dw_ventes_fk_dat;
@@ -10,6 +12,8 @@ drop index f_dw_date_idx_dat;
 drop index f_dw_magasin_idx_num_mag;
 drop index f_dw_produit_idx_isbn;
 
+prompt
+prompt rempli la table f_dw_ventes
 insert into nf26p008.f_dw_ventes (
     magasin,
     dat,
@@ -21,10 +25,11 @@ select
     v.getDat(),
     v.getProduit(),
     v.getTicket()
-from nf26p008.f_bdt_ventes v
-;
+from nf26p008.f_bdt_ventes v;
 commit;
 
+prompt
+prompt rempli la table f_dw_magasin
 insert into nf26p008.f_dw_magasin (
     num_mag,
     rayon_bs,
@@ -34,18 +39,19 @@ insert into nf26p008.f_dw_magasin (
     dpt_pop,
     rayonnage
 )
-    select
-        m.getNumMag(),
-        m.getRayonBs(),
-        m.getRayonRecent(),
-        m.getDpt(),
-        m.getDptNom(),
-        m.getDptPop(),
-        m.getRayonnage()
-    from nf26p008.f_bdt_magasin m
-;
+select
+    m.getNumMag(),
+    m.getRayonBs(),
+    m.getRayonRecent(),
+    m.getDpt(),
+    m.getDptNom(),
+    m.getDptPop(),
+    m.getRayonnage()
+from nf26p008.f_bdt_magasin m;
 commit;
 
+prompt
+prompt rempli la table f_dw_produit
 insert into nf26p008.f_dw_produit (
     isbn,
     titre,
@@ -55,18 +61,19 @@ insert into nf26p008.f_dw_produit (
     editeur,
     parution
 )
-    select
-        p.getIsbn(),
-        p.getTitre(),
-        p.getLangue(),
-        p.getGenre(),
-        p.getAuteur(),
-        p.getEditeur(),
-        p.getParution()
-   from nf26p008.f_bdt_catalogue p
-;
+select
+    p.getIsbn(),
+    p.getTitre(),
+    p.getLangue(),
+    p.getGenre(),
+    p.getAuteur(),
+    p.getEditeur(),
+    p.getParution()
+from nf26p008.f_bdt_catalogue p;
 commit;
 
+prompt
+prompt rempli la table f_dw_date
 insert into nf26p008.f_dw_date (
     dat,
     jour_semaine,
@@ -74,17 +81,18 @@ insert into nf26p008.f_dw_date (
     semaine,
     trimestre
 )
-    select distinct
-        d.getDat(),
-        d.getJourSemaine(),
-        d.getMois(),
-        d.getSemaine(),
-        d.getTrimestre()
-    from nf26p008.f_bdt_date d
-    where d.getDat() is not null
-;
+select distinct
+    d.getDat(),
+    d.getJourSemaine(),
+    d.getMois(),
+    d.getSemaine(),
+    d.getTrimestre()
+from nf26p008.f_bdt_date d
+where d.getDat() is not null;
 commit;
 
+prompt
+prompt recree les indexes et les constraints
 create unique index f_dw_produit_idx_isbn on nf26p008.f_dw_produit(isbn);
 create unique index f_dw_magasin_idx_num_mag on nf26p008.f_dw_magasin(num_mag);
 create unique index f_dw_date_idx_dat on nf26p008.f_dw_date(dat);
@@ -103,16 +111,30 @@ alter table nf26p008.f_dw_ventes enable constraint f_dw_ventes_fk_produit
 alter table nf26p008.f_dw_ventes enable constraint f_dw_ventes_fk_dat
     exceptions into nf26p008.exceptions;
 
+prompt
+prompt count(*) de exceptions
 select count(*) from nf26p008.exceptions;
 
+prompt
+prompt 10 premieres lignes de f_dw_ventes
 select * from nf26p008.f_dw_ventes where rownum <= 10;
+prompt count(*) de f_dw_ventes
 select count(*) from nf26p008.f_dw_ventes;
 
+prompt
+prompt 10 premieres lignes de f_dw_date
 select * from nf26p008.f_dw_date where rownum <= 10;
+prompt count(*) de f_dw_date
 select count(*) from nf26p008.f_dw_date;
 
+prompt
+prompt 10 premieres lignes de f_dw_magasin
 select * from nf26p008.f_dw_magasin where rownum <= 10;
+prompt count(*) de f_dw_magasin
 select count(*) from nf26p008.f_dw_magasin;
 
+prompt
+prompt 10 premieres lignes de f_dw_produit
 select * from nf26p008.f_dw_produit where rownum <= 10;
+prompt count(*) de f_dw_produit
 select count(*) from nf26p008.f_dw_produit;
