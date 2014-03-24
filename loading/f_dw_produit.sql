363e2fac89ec90448487079d547b575da55ae7cd
Mooduku <julien.amalfi@gmail.com>
2014-03-24 16:12:38 +0100

    Update f_dw_produit.sql

--- loading/f_dw_produit.sql
+++ loading/f_dw_produit.sql
@@ -2,7 +2,7 @@ create table nf26p008.f_dw_produit (
     isbn varchar(13),
     titre varchar(255),
     langue char(3),
-    genre varchar(50),
+    genre varchar(255),
     auteur varchar(255),
     editeur varchar(50),
     parution char(4)
