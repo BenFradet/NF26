be7cd7f7cb1e07dff9a51ebb1a2eafb3550e23cd
Benjamin Fradet <benjamin.fradet@gmail.com>
2014-04-14 15:57:55 +0200

    dirs

--- ETL-Dune/extraction/d_bde_dirs.sql
+++ ETL-Dune/extraction/d_bde_dirs.sql
@@ -1,4 +1,4 @@
 create or replace directory nf26p008Tmp as 
     '/volsme/users/nf26p008/Documents/tmp';
  
-create or replace directory nf26p008Source as '/home/nf26/data';
+create or replace directory nf26p008Source as '/home/nf26/dune/8';
