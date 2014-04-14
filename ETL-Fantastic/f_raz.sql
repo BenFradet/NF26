41960cee906b9aa62bd05eff2f277b383dd7f63b
BenFradet <benjamin.fradet@gmail.com>
2014-04-14 14:46:42 +0200

    creation etl-dune

---
+++ ETL-Fantastic/f_raz.sql
@@ -0,0 +1,22 @@
+begin
+    for c in (select type_name from user_types) loop
+    execute immediate ('drop type nf26p008.' || c.type_name || ' force');
+    end loop;
+end;
+/
+begin
+    for c in 
+        (select object_name from user_objects where object_type='TABLE') loop
+    execute immediate ('drop table nf26p008.' || c.object_name || 
+        ' cascade constraints');
+    end loop;
+end;
+/
+begin
+  for c in
+      (select object_name from user_objects where object_type='VIEW') loop
+  execute immediate ('drop view nf26p008.' || c.object_name ||
+      ' cascade constraints');
+  end loop;
+end;
+/
